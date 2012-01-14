44cc5aa1810f96616fa7369f4fe18b231785cee5
Silas Sewell <silas@sewell.org>
2012-01-13 19:46:30 -0500

    Change test.hasName/test.hasAddress to test.name/test.address

--- test/dns.js
+++ test/dns.js
@@ -6,60 +6,60 @@ exports.asserts = function(test) {
   test.done()
 }
 
-exports.assertsHasAddress = function(test) {
+exports.assertsAddress = function(test) {
   var address = '127.0.0.1'
     , mockTest = new mock.AssertTest(test, dns)
 
-  mockTest.hasAddress(true, address)
+  mockTest.address(true, address)
   mockTest.assertOk(false)
 
-  mockTest.hasAddress({}, address)
+  mockTest.address({}, address)
   mockTest.assertOk(false)
 
-  mockTest.hasAddress({ type: 'a', answer: [] }, address)
+  mockTest.address({ type: 'a', answer: [] }, address)
   mockTest.assertOk(false)
 
-  mockTest.hasAddress({ type: 'a', answer: '127.0.0.1' }, address)
+  mockTest.address({ type: 'a', answer: '127.0.0.1' }, address)
   mockTest.assertOk(false)
 
-  mockTest.hasAddress({ answer: ['127.0.0.1'] }, address)
+  mockTest.address({ answer: ['127.0.0.1'] }, address)
   mockTest.assertOk(false)
 
-  mockTest.hasAddress({ type: 'a', answer: ['127.0.0.1'] }, address)
+  mockTest.address({ type: 'a', answer: ['127.0.0.1'] }, address)
   mockTest.assertOk()
 
-  mockTest.hasAddress({ type: 'aaaa', answer: ['127.0.0.1'] }, address)
+  mockTest.address({ type: 'aaaa', answer: ['127.0.0.1'] }, address)
   mockTest.assertOk()
 
   test.done()
 }
 
-exports.assertsHasName = function(test) {
+exports.assertsName = function(test) {
   var name = 'example.org'
     , mockTest = new mock.AssertTest(test, dns)
 
-  mockTest.hasName(true, name)
+  mockTest.name(true, name)
   mockTest.assertOk(false)
 
-  mockTest.hasName({}, name)
+  mockTest.name({}, name)
   mockTest.assertOk(false)
 
-  mockTest.hasName({ type: 'cname', answer: [] }, name)
+  mockTest.name({ type: 'cname', answer: [] }, name)
   mockTest.assertOk(false)
 
-  mockTest.hasName({ type: 'cname', answer: 'example.org' }, name)
+  mockTest.name({ type: 'cname', answer: 'example.org' }, name)
   mockTest.assertOk(false)
 
-  mockTest.hasName({ answer: ['example.org'] }, name)
+  mockTest.name({ answer: ['example.org'] }, name)
   mockTest.assertOk(false)
 
-  mockTest.hasName({ type: 'cname', answer: ['example.org'] }, name)
+  mockTest.name({ type: 'cname', answer: ['example.org'] }, name)
   mockTest.assertOk()
 
-  mockTest.hasName({ type: 'ns', answer: ['example.org'] }, name)
+  mockTest.name({ type: 'ns', answer: ['example.org'] }, name)
   mockTest.assertOk()
 
-  mockTest.hasName({ type: 'ptr', answer: ['example.org'] }, name)
+  mockTest.name({ type: 'ptr', answer: ['example.org'] }, name)
   mockTest.assertOk()
 
   test.done()
