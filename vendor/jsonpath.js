32da324779fa3146b5f4bcdf5fa5ee2566d23e37
Silas Sewell <silas@sewell.org>
2012-04-18 00:29:07 -0400

    Make jshint happy

---
+++ vendor/jsonpath.js
@@ -0,0 +1,132 @@
+/* JSONPath 0.8.0 - XPath for JSON
+ *
+ * Copyright (c) 2007 Stefan Goessner (goessner.net)
+ * Licensed under the MIT (MIT-LICENSE.txt) licence.
+ */
+
+var vm = require('vm');
+module.exports = jsonPath;
+var cache = {};
+function jsonPath(obj, expr, arg) {
+   var P = {
+      resultType: arg && arg.resultType || "VALUE",
+      flatten: arg && arg.flatten || false,
+      wrap: (arg && arg.hasOwnProperty('wrap')) ? arg.wrap : true,
+      sandbox: (arg && arg.sandbox) ? arg.sandbox : {},
+      normalize: function(expr) {
+         if(cache[expr]) {
+            return cache[expr];
+         }
+
+         var subx = [];
+         ret = expr.replace(/[\['](\??\(.*?\))[\]']/g, function($0,$1){return "[#"+(subx.push($1)-1)+"]";})
+                    .replace(/'?\.'?|\['?/g, ";")
+                    .replace(/;;;|;;/g, ";..;")
+                    .replace(/;$|'?\]|'$/g, "")
+                    .replace(/#([0-9]+)/g, function($0,$1){return subx[$1];});
+         cache[expr] = ret;
+         return ret;
+      },
+      asPath: function(path) {
+         var x = path.split(";"), p = "$";
+         for (var i=1,n=x.length; i<n; i++)
+            p += /^[0-9*]+$/.test(x[i]) ? ("["+x[i]+"]") : ("['"+x[i]+"']");
+         return p;
+      },
+      store: function(p, v) {
+         if (p) {
+             if (P.resultType == "PATH") {
+                 P.result[P.result.length] = P.asPath(p);
+             }
+             else {
+                 if(Array.isArray(v) && P.flatten) {
+                     if(!P.result) P.result = [];
+                     if(!Array.isArray(P.result)) P.result = [P.result];
+                     P.result = P.result.concat(v);
+                 }
+                 else {
+                     if(P.result) {
+                         if(!Array.isArray(P.result)) P.result = [P.result];
+                         if(Array.isArray(v) && P.flatten) {
+                             P.result = P.result.concat(v);
+                         }
+                         else {
+                             P.result[P.result.length] = v;
+                         }
+                     }
+                     else {
+                         P.result = v;
+                     }
+                 }
+             }
+         }
+         return !!p;
+      },
+      trace: function(expr, val, path) {
+         if (expr) {
+            var x = expr.split(";"), loc = x.shift();
+            x = x.join(";");
+            if (val && val.hasOwnProperty(loc))
+               P.trace(x, val[loc], path + ";" + loc);
+            else if (loc === "*")
+               P.walk(loc, x, val, path, function(m,l,x,v,p) { P.trace(m+";"+x,v,p); });
+            else if (loc === "..") {
+               P.trace(x, val, path);
+               P.walk(loc, x, val, path, function(m,l,x,v,p) { if (typeof v[m] === "object") P.trace("..;"+x,v[m],p+";"+m); });
+            }
+            else if (/,/.test(loc)) { // [name1,name2,...]
+               for (var s=loc.split(/'?,'?/),i=0,n=s.length; i<n; i++)
+                  P.trace(s[i]+";"+x, val, path);
+            }
+            else if (/^\(.*?\)$/.test(loc)) // [(expr)]
+               P.trace(P.run(loc, val, path.substr(path.lastIndexOf(";")+1))+";"+x, val, path);
+            else if (/^\?\(.*?\)$/.test(loc)) // [?(expr)]
+               P.walk(loc, x, val, path, function(m,l,x,v,p) { if (P.run(l.replace(/^\?\((.*?)\)$/,"$1"),v[m],m)) P.trace(m+";"+x,v,p); });
+            else if (/^(-?[0-9]*):(-?[0-9]*):?([0-9]*)$/.test(loc)) // [start:end:step]  phyton slice syntax
+               P.slice(loc, x, val, path);
+         }
+         else
+            P.store(path, val);
+      },
+      walk: function(loc, expr, val, path, f) {
+         if (val instanceof Array) {
+            for (var i=0,n=val.length; i<n; i++)
+               if (i in val)
+                  f(i,loc,expr,val,path);
+         }
+         else if (typeof val === "object") {
+            for (var m in val)
+               if (val.hasOwnProperty(m))
+                  f(m,loc,expr,val,path);
+         }
+      },
+      slice: function(loc, expr, val, path) {
+         if (val instanceof Array) {
+            var len=val.length, start=0, end=len, step=1;
+            loc.replace(/^(-?[0-9]*):(-?[0-9]*):?(-?[0-9]*)$/g, function($0,$1,$2,$3){start=parseInt($1||start, 10);end=parseInt($2||end, 10);step=parseInt($3||step, 10);});
+            start = (start < 0) ? Math.max(0,start+len) : Math.min(len,start);
+            end   = (end < 0)   ? Math.max(0,end+len)   : Math.min(len,end);
+            for (var i=start; i<end; i+=step)
+               P.trace(i+";"+expr, val, path);
+         }
+      },
+      run: function(x, _v, _vname) {
+         P.sandbox._v = _v;
+         try {
+             return $ && _v && vm.runInNewContext(x.replace(/@/g, "_v"), P.sandbox);
+         }
+         catch(e) {
+             console.log(e);
+             throw new SyntaxError("jsonPath: " + e.message + ": " + x.replace(/@/g, "_v").replace(/\^/g, "_a"));
+         }
+      }
+   };
+   P.result = P.wrap === true ? [] : undefined;
+
+   var $ = obj;
+   if (expr && obj && (P.resultType == "VALUE" || P.resultType == "PATH")) {
+      P.trace(P.normalize(expr).replace(/^\$;/,""), obj, "$");
+      if(!Array.isArray(P.result) && P.wrap) P.result = [P.result];
+      return P.result ? P.result : false;
+   }
+}
