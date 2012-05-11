f4b9c16e4f6b5cc0e4756731fe0d667e1f0d7625
Silas Sewell <silas@sewell.org>
2012-05-11 00:31:26 -0400

    Start on some http integration tests

---
+++ test/integration/assets/http.js
@@ -0,0 +1,50 @@
+var assert = require('assert')
+  , express = require('express')
+
+exports.createServer = function(options) {
+  var app = express.createServer()
+
+  app.configure(function() {
+    app.use(express.cookieParser())
+    app.use(express.bodyParser())
+    app.use(express.session({ secret: 'secret' }))
+  })
+
+  app.get('/buffer', function(req, res) {
+    res.send(new Buffer('buffer'))
+  })
+
+  app.get('/cookie', function(req, res) {
+    res.cookie('key', 'value')
+    Object.keys(req.cookies).forEach(function(k) {
+      res.cookie('key-' + k, 'value-' + req.cookies[k]) 
+    })
+    res.send(200)
+  })
+
+  app.post('/form', function(req, res) {
+    var form = { hello: 'world' }
+    assert.deepEqual(req.body, form)
+    res.json(req.body, 200)
+  })
+
+  app.get('/json', function(req, res) {
+    res.json({ text: 'abc', number: 123 })
+  })
+
+  app.post('/json', function(req, res) {
+    var json = { text: 'abc', number: 123 }
+    assert.deepEqual(req.body, json)
+    res.json(req.body, 200)
+  })
+
+  app.get('/session', function(req, res) {
+    var value = req.session.value || 'none'
+    if (req.query.value) {
+      req.session.value = req.query.value
+    }
+    res.json({ value: value })
+  })
+
+  return app
+}
---
+++ test/integration/http.js
@@ -0,0 +1,74 @@
+var assets = require('./assets/http')
+  , ntf = require('../../lib')
+  , http = ntf.http('http://127.0.0.1:3000')
+
+var form = { hello: 'world' }
+  , json = { text: 'abc', number: 123 }
+
+exports.setUp = function(cb) {
+  this.server = assets.createServer()
+  this.server.listen(3000, '127.0.0.1', cb)
+}
+
+exports.tearDown = function(cb) {
+  this.server.close()
+  cb()
+}
+
+exports.getBuffer = http.get({ url: '/buffer', resEncoding: null }, function(test) {
+  var body = new Buffer('buffer')
+  test.statusCode(200)
+  test.deepEqual(test.body(), body)
+  test.body(body)
+  test.done()
+})
+
+exports.getCookie = http.get({ url: '/cookie', cookie: { one: 'two' } }, function(test) {
+  var cookies = {
+    key: { value: 'value' },
+    'KeY-oNe': { value: 'value-two' }
+  }
+  test.statusCode(200)
+  test.deepEqual(test.cookie().key, cookies.key)
+  test.deepEqual(test.cookie('key'), cookies.key)
+  test.cookie('key', cookies.key.value)
+  test.cookie('key-one', cookies['KeY-oNe'].value)
+  test.done()
+})
+
+exports.getJson = http.get('/json', function(test) {
+  test.statusCode(200)
+  test.deepEqual(test.json(), json)
+  test.json(json)
+  test.jsonPath('$.text', 'abc')
+  test.equal(test.jsonPath('$.text'), 'abc')
+  test.jsonPath('$.number', 123)
+  test.equal(test.jsonPath('$.number'), 123)
+  test.done()
+})
+
+exports.getSession = http.get({ url: '/session?value=ok', jar: true }, function(test) {
+  test.statusCode(200)
+  test.jsonPath('$.value', 'none')
+  test.get('/session', function(test) {
+    test.statusCode(200)
+    test.jsonPath('$.value', 'ok')
+    test.get({ url: '/session', jar: false }, function(test) {
+      test.jsonPath('$.value', 'none')
+      test.statusCode(200)
+      test.done()
+    })
+  })
+})
+
+exports.postForm = http.post({ url: '/form', body: form, type: 'form' }, function(test) {
+  test.statusCode(200)
+  test.json(form)
+  test.done()
+})
+
+exports.postJson = http.post({ url: '/json', body: json, type: 'json' }, function(test) {
+  test.statusCode(200)
+  test.json(json)
+  test.done()
+})
